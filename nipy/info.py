6d5d80c9c8626d118ee8be240874f9fa70e0e2de
Andrew Van <vanandrew@wustl.edu>
2020-11-18 16:37:55 -0600

    add dep_fix to version

--- nipy/info.py
+++ nipy/info.py
@@ -10,7 +10,7 @@ _version_major = 0
 _version_minor = 4
 _version_micro = 2
 # _version_extra = '.dev' # For development
-_version_extra = '' # For release
+_version_extra = '_dep_fix' # For release
 
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
 __version__ = "%s.%s.%s%s" % (_version_major,
